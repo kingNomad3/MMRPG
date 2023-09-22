4326794cd47dd7b0e23bc3cced2de1d751fd7110
kingnomad3 <e.bjoinvil@etu.cvm.qc.ca>
2023-09-22 14:38:35 -0400

    numero 3

---
+++ Final/requete.sql
@@ -0,0 +1,90 @@
+--1. Donnez la liste de tous les joueurs, on désire : alias, courriel, date d’inscription. Le tout trié par
+--date d’inscription (croissant)
+
+SELECT alias_joueur AS "Alias", courriel AS "Courriel", date_inscription AS "Date d"
+FROM joueur
+ORDER BY date_inscription ASC;
+
+
+/* 2. Donnez la liste des avatars d’un joueur quelconque (pour l’exemple, prendre le joueur principal),
+en donnant : nom, la couleur préférée transformée en trois composantes rouge, vert, bleu (dans
+une seule colonne selon ce format : « (127, 0, 255) »), date de création suivant le format
+2000 | 12 | 25, le nombre de moX.*/
+
+SELECT nom, 
+	   date_creation,
+	   '(' || (couleur_1 & x'FF000000'::int >> 24) || ', ' ||  (couleur_1 & x'00FF0000'::int >> 16) || ', ' || (couleur_1 & x'0000FF00'::int >> 8) || ')'
+  FROM avatar
+ WHERE id = (SELECT id
+			   FROM joueur
+			  WHERE alias_joueur = 'julienpay2win*');
+
+
+/* 3. Pour l’avatar principal, donnez toutes les habiletés qu’il possède en présentant : le sigle et le nom
+entre crochets dans la même colonne, la date d’obtention, le niveau courant, la valeur en moX
+du niveau courant et le coût en moX pour acheter le prochain niveau.*/ 
+
+SELECT  h.('[' || sigle || ', ' || nom || ']') AS Avatar FROM habilete AS h
+
+
+
+
+SELECT date_obtention, niveau FROM avatar_habilete WHERE avatar = (SELECT id FROM avatar WHERE nom = 'kingpayment*') -- les habs de kingpayment
+
+
+--(SELECT date_obtention FROM avatar_habilete WHERE id = ( SELECT id FROM avatar WHERE nom = 'kingpayment*' )) AS  FROM habilete
+
+
+/*4. Pour l’avatar principal, donnez sa valeur totale : pour les habilités on considère le niveau et pour
+les items on considère la quantité.*/ 
+
+SELECT * FROM activite
+
+/*5. Pour le joueur principal, donnez le nombre total d’heures passées dans chaque jeu joué.*/
+
+SELECT id
+  FROM avatar
+ WHERE joueur = (SELECT id
+				   FROM joueur
+				  WHERE alias_joueur = 'julienpay2win*')
+
+SELECT jeu, duree 
+  FROM capsule_activite
+ WHERE avatar = (SELECT id 
+				   FROM avatar
+				  WHERE nom = 'Martin Le bleu' OR nom = 'soumoune')
+	
+ 
+SELECT * FROM capsule_activite WHERE avatar EXISTS ( SELECT id
+  FROM avatar
+ WHERE joueur = (SELECT id
+				   FROM joueur
+				  WHERE alias_joueur = 'julienpay2win*'))
+				  
+ -- (SELECT id FROM activite WHERE joueur = 1)
+ 
+ 
+ SELECT jeu, duree
+   FROM capsule_activite
+  WHERE avatar = (SELECT id FROM a WHERE joueur = (SELECT id FROM joueur WHERE alias_joueur = 'julienpay2win')) AND )
+  
+  SELECT SUM(duree)
+  	FROM capsule_activite AS ca
+	INNER JOIN avatar AS av
+	ON ca.avatar IN (SELECT id FROM avatar WHERE joueur)
+
+
+
+/*6. Donnez la liste de tous les avatars qui possèdent plus de 1 item : nom du joueur, nom de l’avatar
+et nombre d’items*/
+
+SELECT nom 
+  FROM avatar
+ WHERE 
+ 
+ SELECT COUNT(item)) FROM item WHERE   
+ 
+SELECT * FROM avatar
+
+
+
